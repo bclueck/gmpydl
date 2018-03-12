55e186cba84933a28fa1023e75d1fed786303c0e
brian <brian.lueck@gmail.com>
2018-03-11 20:00:11 -0500

    Update

--- gmpydl.py
+++ gmpydl.py
@@ -202,7 +202,7 @@ def download_song(api, sid, update_dl):
             try:
                 filename, audio = api.download_song(song['id'])
                 filepath = os.path.join(path, filename)
-            except RuntimeError:
+            except gmusicapi.exceptions.CallFailure:
                 log("Not availabe")
                 return True
             with open(filepath, 'wb') as f:
